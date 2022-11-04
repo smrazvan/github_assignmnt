b3af063e374d1e7162bf441e46be33731fe78e57
smrazvan <razvanro666@gmail.com>
2022-11-04 14:39:17 +0200

    testing ssh

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ let name = "john";
 
 let friend = "mariana";
 
-const showBestFriend = (name, bf) => console.log(`I am ${name}, wanna hangout ${bf}?`);
+const showBestFriend = (name, bf) =>
+  console.log(`I am ${name}, wanna hangout ${bf}?`);
 
 let secondFriend = "robert";
 
 let thirdFriend = "george";
 let friendsMeetupSpot = "New York";
+
+// test to see ssh
